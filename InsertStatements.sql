4a9eb0299a2414523d281a9fe4209b59c206915d
Dheeraj Tommandru <95251008+tommandru@users.noreply.github.com>
2022-09-09 06:13:25 -0400

    Update InsertStatements.sql

--- InsertStatements.sql
+++ InsertStatements.sql
@@ -65,14 +65,22 @@ SELECT * FROM dual;
 
 MERGE INTO OAO_DEVELOPMENT.summary_repo_example  SR
 USING OAO_DEVELOPMENT.merge_example SOURCE_TABLE
+
+-- Specify fields to be joined on
+
 ON (  SR."SITE" = SOURCE_TABLE."SITE" AND
       SR."SERVICE" = SOURCE_TABLE."SERVICE" AND
       SR."METRIC_NAME_SUBMITTED" = SOURCE_TABLE."METRIC_NAME_SUBMITTED" AND
       SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD")
+-- Specify what fields to be updated when the records match
+
 WHEN MATCHED THEN 
 UPDATE  SET SR."VALUE" = SOURCE_TABLE."VALUE",
             SR."UPDATE_TIME" = SOURCE_TABLE."UPDATE_TIME",
             SR."UPDATE_USER" = SOURCE_TABLE."UPDATE_USER"
+	    
+-- Specify what fields to insert when the records don't match
+
 WHEN NOT MATCHED THEN
 INSERT( SR."SITE",
         SR."SERVICE",
@@ -88,4 +96,44 @@ VALUES( SOURCE_TABLE."SITE",
         SOURCE_TABLE."UPDATE_TIME",
         SOURCE_TABLE."UPDATE_USER",
         SOURCE_TABLE."PREMIER_REPORTING_PERIOD");
+	
+-- Variant of MERGE to update only when values don't match
+
+MERGE INTO OAO_DEVELOPMENT.summary_repo_example  SR
+USING OAO_DEVELOPMENT.merge_example SOURCE_TABLE
+
+-- Specify fields to be joined on
+
+ON (  SR."SITE" = SOURCE_TABLE."SITE" AND
+      SR."SERVICE" = SOURCE_TABLE."SERVICE" AND
+      SR."METRIC_NAME_SUBMITTED" = SOURCE_TABLE."METRIC_NAME_SUBMITTED" AND
+      SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD")
+-- Specify what fields to be updated when the records match
+
+WHEN MATCHED THEN 
+UPDATE  SET SR."VALUE" = SOURCE_TABLE."VALUE",
+            SR."UPDATE_TIME" = SOURCE_TABLE."UPDATE_TIME",
+            SR."UPDATE_USER" = SOURCE_TABLE."UPDATE_USER"
+-- WHERE clause prevents the all the records from getting merged
+
+WHERE SR."VALUE" <> SOURCE_TABLE."VALUE"
+
+-- Specify fields to be joined on
+
+WHEN NOT MATCHED THEN
+INSERT( SR."SITE",
+        SR."SERVICE",
+        SR."METRIC_NAME_SUBMITTED",
+        SR."VALUE", 
+        SR."UPDATE_TIME", 
+        SR."UPDATE_USER",
+        SR."PREMIER_REPORTING_PERIOD")  
+VALUES( SOURCE_TABLE."SITE",
+        SOURCE_TABLE."SERVICE",
+        SOURCE_TABLE."METRIC_NAME_SUBMITTED",
+        SOURCE_TABLE."VALUE", 
+        SOURCE_TABLE."UPDATE_TIME",
+        SOURCE_TABLE."UPDATE_USER",
+        SOURCE_TABLE."PREMIER_REPORTING_PERIOD");
+
 
