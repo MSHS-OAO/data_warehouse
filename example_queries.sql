72ff3d6f1b68d0dfc3d6308051ae9c151bf07809
armando-v <57226186+armando-v@users.noreply.github.com>
2022-09-12 14:40:03 -0400

    Update example_queries.sql

--- example_queries.sql
+++ example_queries.sql
@@ -1,6 +1,4 @@
---- insert statements start here
-/* Basic insert statment*/
-
+-- BASIC INSERT STATEMENT /SLIDE 12
 INSERT INTO OAO_DEVELOPMENT.summary_repo_example
 (SERVICE,
 SITE,
@@ -19,7 +17,7 @@ VALUES('Lab',
 	    'Jan 2020',
 	    'NA');
 	    
-/* Insert statment from select example*/
+-- Insert statment from select example / SLIDE 12
 /* 
 Note on DBMS_RANDOM.RANDOM : https://asktom.oracle.com/pls/apex/f?p=100:11:0::::P11_QUESTION_ID:1859804900346710586
 				https://datacadamia.com/lang/plsql/dbms_random 
@@ -37,19 +35,56 @@ FROM   (
     ORDER BY DBMS_RANDOM.RANDOM)
 WHERE  rownum < 21;
 	  
+-- Insert all statment example / SLIDE 13
+	
+INSERT ALL 
+INTO OAO_DEVELOPMENT.summary_repo_example (SERVICE,
+					   SITE,
+					   MONTH,
+				   	   METRIC_NAME_SUBMITTED,
+					   VALUE,
+                       			   PREMIER_REPORTING_PERIOD)
+VALUES('Lab',
+       'MSB',
+        TO_DATE('2020-01-01','YYYY-MM-DD'),
+       'Troponin (<=50 min)',
+	0.8913,
+       'Jan 2020')
+INTO OAO_DEVELOPMENT.merge_example (SERVICE,
+					   SITE,
+					   MONTH,
+				   	   METRIC_NAME_SUBMITTED,
+					   VALUE,
+                       			   PREMIER_REPORTING_PERIOD)
+VALUES('Lab',
+       'MSB',
+        TO_DATE('2020-01-01','YYYY-MM-DD'),
+       'Troponin (<=50 min)',
+	0.8913,
+       'Jan 2020')				
+SELECT * FROM dual; 
 
-/* Merge statment example*/
+
+-- Merge statment example / SLIDE 15
 
 MERGE INTO OAO_DEVELOPMENT.summary_repo_example  SR
 USING OAO_DEVELOPMENT.merge_example SOURCE_TABLE
+
+-- Specify fields to be joined on
+
 ON (  SR."SITE" = SOURCE_TABLE."SITE" AND
       SR."SERVICE" = SOURCE_TABLE."SERVICE" AND
       SR."METRIC_NAME_SUBMITTED" = SOURCE_TABLE."METRIC_NAME_SUBMITTED" AND
       SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD")
+-- Specify what fields to be updated when the records match
+
 WHEN MATCHED THEN 
 UPDATE  SET SR."VALUE" = SOURCE_TABLE."VALUE",
             SR."UPDATE_TIME" = SOURCE_TABLE."UPDATE_TIME",
             SR."UPDATE_USER" = SOURCE_TABLE."UPDATE_USER"
+	    
+-- Specify what fields to insert when the records don't match
+
 WHEN NOT MATCHED THEN
 INSERT( SR."SITE",
         SR."SERVICE",
@@ -65,31 +100,47 @@ VALUES( SOURCE_TABLE."SITE",
         SOURCE_TABLE."UPDATE_TIME",
         SOURCE_TABLE."UPDATE_USER",
         SOURCE_TABLE."PREMIER_REPORTING_PERIOD");
-
-/* Insert all statment example*/
 	
-INSERT ALL 
-INTO OAO_DEVELOPMENT.summary_repo_example (SERVICE,
-					   SITE,
-					   MONTH,
-				   	   METRIC_NAME_SUBMITTED,
-					   VALUE)
-VALUES('Lab',
-       'MSB',
-        TO_DATE('2020-01-01','YYYY-MM-DD'),
-       'Troponin (<=50 min)',
-	0.8913)
-INTO OAO_DEVELOPMENT.merge_example (SERVICE,
-					   SITE,
-					   MONTH,
-				   	   METRIC_NAME_SUBMITTED,
-					   VALUE)
-VALUES('Lab',
-       'MSB',
-        TO_DATE('2020-01-01','YYYY-MM-DD'),
-       'Troponin (<=50 min)',
-	0.8913)					
-SELECT * FROM dual; 
+-- Variant of MERGE to update only when values don't match
+
+MERGE INTO OAO_DEVELOPMENT.summary_repo_example  SR
+USING OAO_DEVELOPMENT.merge_example SOURCE_TABLE
+
+-- Specify fields to be joined on
+
+ON (  SR."SITE" = SOURCE_TABLE."SITE" AND
+      SR."SERVICE" = SOURCE_TABLE."SERVICE" AND
+      SR."METRIC_NAME_SUBMITTED" = SOURCE_TABLE."METRIC_NAME_SUBMITTED" AND
+      SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD")
+-- Specify what fields to be updated when the records match
+
+WHEN MATCHED THEN 
+UPDATE  SET SR."VALUE" = SOURCE_TABLE."VALUE",
+            SR."UPDATE_TIME" = SOURCE_TABLE."UPDATE_TIME",
+            SR."UPDATE_USER" = SOURCE_TABLE."UPDATE_USER"
+-- WHERE clause prevents the all the records from getting merged
+
+WHERE SR."VALUE" <> SOURCE_TABLE."VALUE"
+
+-- Specify fields to be joined on
+
+WHEN NOT MATCHED THEN
+INSERT( SR."SITE",
+        SR."SERVICE",
+        SR."METRIC_NAME_SUBMITTED",
+        SR."VALUE", 
+        SR."UPDATE_TIME", 
+        SR."UPDATE_USER",
+        SR."PREMIER_REPORTING_PERIOD",
+        SR."MONTH");  
+VALUES( SOURCE_TABLE."SITE",
+        SOURCE_TABLE."SERVICE",
+        SOURCE_TABLE."METRIC_NAME_SUBMITTED",
+        SOURCE_TABLE."VALUE", 
+        SOURCE_TABLE."UPDATE_TIME",
+        SOURCE_TABLE."UPDATE_USER",
+        SOURCE_TABLE."PREMIER_REPORTING_PERIOD",
+        SOURCE_TABLE."MONTH");
 
 --- select all records from a specified table
 SELECT * FROM OAO_DEVELOPMENT.summary_repo_example;
