87c2255b0df40269b62f7ffd0f5fa14b44cb5031
Villegas <armando.villegas@mssm.edu>
2022-12-14 11:34:29 -0500

    add manifest file

--- rsql_example.R
+++ rsql_example.R
@@ -4,7 +4,7 @@ library(glue)
 library(dbplyr)
 library(dplyr)
 
-#Explain the difference betwween Rwokrbench and local including speed
+#Explain the difference betwween Rworkbench and local including speed
 dsn <- "OAO Cloud DB" #Explain that this will be different for everyone
 conn <- dbConnect(odbc(), dsn)
 
@@ -13,7 +13,7 @@ date_1 <- "2022-11-01"
 date_2 <- "2022-11-01"
 
 ## Glue is more felxible we can create insert statments
-query <- glue("SELECT DERIVED_STATUS_DESC , CONTACT_DATE FROM \"{table}\" WHERE DERIVED_STATUS_DESC  = 'Canceled' AND 
+query <- glue("SELECT DERIVED_STATUS_DESC , CONTACT_DATE FROM \"{table}\" WHERE DERIVED_STATUS_DESC  = 'Canceled' AND
               CONTACT_DATE BETWEEN TO_DATE('{date_1} 00:00:00', 'YYYY-MM-DD HH24:MI:SS')
                         AND TO_DATE('{date_2} 23:59:59', 'YYYY-MM-DD HH24:MI:SS')")
 
